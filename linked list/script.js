b6a35a44b2a2a26a7decefc32e71f35a18173cf5
Serjik07 <serjik.h1@tumo.org>
2023-02-12 17:20:19 +0400

    remove is redy

--- linked list/script.js
+++ linked list/script.js
@@ -7,34 +7,48 @@ class Node {
 
 class LinkedLink {
     constructor(){
-        this.head = null
-        this.tail = null
+        this.head = null;
+        this.tail = null;
         
     }
 
     add(num) {
-        debugger;
        let data = new Node(num);
        if(!this.head) {
            this.head = data; 
-           this.tail = data
+           this.tail = data;
         } else {
-           this.tail.next = data
-           this.tail = data
+           this.tail.next = data;
+           this.tail = data;
         }
     }
     remove() {
-        if(this.head){
-
+        if(!this.tail.next){
+            debugger;
+            // console.log(this.tail);
+            // console.log(this.head);
+            let vlod = this.head;
+            if (vlod.next === null) {
+                return null;
+            }
+            while(vlod.next !== this.tail){
+                vlod = vlod.next;
+            }   
+            vlod.next = null;
+            return this.head;
         }
     }
     print() {
-        console.log(this.head)
+        console.log(this.head);
     }
 }
 
 let x = new LinkedLink();
+
 x.add(2);
 x.add("t");
 x.add(10);
+x.add("g");
+x.add(10);
+x.remove();
 x.print();
